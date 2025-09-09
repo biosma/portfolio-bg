9fc0c69d2520792f17ea58482125b5badd2a78b7
gonzalourielbonelli <gonzalourielbonelli@tradenethub.com>
2025-09-09 11:38:51 -0300

    navigation on projects navbar fixed

--- src/modules/home/ui/components/navbar.tsx
+++ src/modules/home/ui/components/navbar.tsx
@@ -58,10 +58,10 @@ const NavbarItem = ({
 };
 
 const projects = [
-  { id: 1, title: 'TradeNetHub', url: 'work/tradenethub' },
-  { id: 2, title: 'Mena Homes', url: 'work/mena-homes' },
-  { id: 3, title: 'Mentis', url: 'work/mentis' },
-  { id: 4, title: 'Recovery Delivered', url: 'work/recovery-delivered' },
+  { id: 1, title: 'TradeNetHub', url: '/work/tradenethub' },
+  { id: 2, title: 'Mena Homes', url: '/work/mena-homes' },
+  { id: 3, title: 'Mentis', url: '/work/mentis' },
+  { id: 4, title: 'Recovery Delivered', url: '/work/recovery-delivered' },
 ];
 
 const navbarItems = [
